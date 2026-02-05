0a149806716c3eee81428e86456454c101fec315
MarleMGN <m0b.torres23@gmail.com>
2026-02-04 17:52:57 -0800

    slideshowfix

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,68 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import Img1 from "../assets/IMG_5857.jpg"
-import Img2 from "../assets/IMG_5752.jpg"
-import Img3 from "../assets/IMG_1655.jpeg"
-import Img4 from "../assets/IMG_5936.GIF"
+import React, { useState, useEffect } from "react";
+import Img1 from "../assets/IMG_5857.jpg";
+import Img2 from "../assets/IMG_5752.jpg";
+import Img3 from "../assets/IMG_1655.jpeg";
+import Img4 from "../assets/IMG_5936.GIF";
+
+const images = [Img1, Img2, Img3, Img4];
 
 const Landing = () => {
-    
-const [slideIndex, setSlideIndex] = useState(1);
+  const [slideIndex, setSlideIndex] = useState(0);
 
-useEffect(() => {
-    showSlides(slideIndex);
-    const interval = setInterval(() => {
-        plusSlides(1);
-    }, 8000);
+  const nextSlide = () => {
+    setSlideIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  const prevSlide = () => {
+    setSlideIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 5000);
     return () => clearInterval(interval);
-}, [slideIndex]);
-
-const plusSlides = (n) => {
-    setSlideIndex((prevIndex) => {
-        let newIndex = prevIndex + n;
-        if (newIndex > plusSlides.length) return 1;
-        if (newIndex < 1) return plusSlides.length;
-        return newIndex;
-    });
-};
-
-const showSlides = (n) => {
-    const slides = document.getElementsByClassName("mySlides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[n - 1].style.display = "block";
-}
-
-
-
+  });
   return (
     <>
-        <header>
-            <div className="container">
-                    <div className="slideshow__container">
-                        <div className="mySlides fade">
-                            <img src={Img1} className="slideshow__img" alt="" />
-                        </div>
-                        <div className="mySlides fade">
-                            <img src={Img2} className="slideshow__img" alt="" />
-                        </div>
-                        <div className="mySlides fade">
-                            <img src={Img3} className="slideshow__img" alt="" />
-                        </div>
-                        <div className="mySlides fade">
-                            <img src={Img4} className="slideshow__img" alt="" />
-                        </div>
-                        <a href="#" className="prev" onclick={plusSlides(-1)}>&#10094;</a>
-                        <a href="#" className="next" onclick={plusSlides(1)}>&#10095;</a>
-                    </div>
-                    <div className="header__description">
-                        <h1 className="header__description--title"></h1>
-                        <p className="header__description--para"></p>
-                    </div>
-            </div>
-        </header>
+      <header>
+        <div className="container">
+          <div className="slideshow__container">
+            <img
+              src={images[slideIndex]}
+              className="slideshow__img fade"
+              alt=""
+            />
+          </div>
+          <button className="prev" onClick={prevSlide}>
+            &#10094; Left
+          </button>
+          <button className="next" onClick={nextSlide}>
+            &#10095; Right
+          </button>
+          <div className="header__description">
+            <h1 className="header__description--title"></h1>
+            <p className="header__description--para"></p>
+          </div>
+        </div>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
